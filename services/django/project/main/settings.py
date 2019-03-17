a35f04a8c61fab197a5e3f59a66fbe3a78aba0ae
nusery_app <app.creche.humaniste@gmail.com>
2019-03-17 17:49:26 +0100

    -- Added Dev Postgres Service --
    + psql service fro development
    + 2 news docker-compose file for each environment
    + refactored run script
    + db config in settings.py

--- services/django/project/main/settings.py
+++ services/django/project/main/settings.py
@@ -105,8 +105,12 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv("DATABASE__NAME"),
+        'USER': os.getenv("DATABASE__USER"),
+        'PASSWORD': os.getenv("DATABASE__PASSWORD"),
+        'HOST': os.getenv("DATABASE__HOST"),
+        'PORT': os.getenv("DATABASE__PORT")
     }
 }
 
